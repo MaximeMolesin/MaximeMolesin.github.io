5b73cc21ad39a18230569f80e52e374354bee02a
Maxime Molesin <maximemolesin@icloud.com>
2025-02-09 11:31:15 +0100

    file next js and deploy.yml

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,10 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
+
+  output: 'export',
+
+  // basePath: '/nom-du-repo',
 }
 
 mergeConfig(nextConfig, userConfig)
