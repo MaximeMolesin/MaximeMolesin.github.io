cdd9ce2d8a6c9629e3f9c055c91f58289084d54e
Maxime Molesin <maximemolesin@icloud.com>
2025-02-10 10:49:31 +0100

    title website

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Maxime Molesin | Engineer, M. Sc.',
+  description: 'Portfolio de Maxime Molesin, Engineer, M. Sc.',
 }
 
 export default function RootLayout({
